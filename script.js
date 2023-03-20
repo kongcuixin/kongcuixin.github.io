7172ce13ce5134e194a17f591fd2e39633900668
kongcuixin <64568748+kongcuixin@users.noreply.github.com>
2023-03-20 14:20:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const email = emailInput.value.trim().toLowerCase();
-  const password = passwordInput.value.trim(); 
+  const password = passwordInput.value.trim();
+
   if (email === "han1306" && password === "adminhan") {
     window.location.href = "index1.html";
   } else {
-    alert("Access denied. Please check your email and password");
+    alert("Incorrect email or password. Please try again.");
   }
 });
