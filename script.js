b5e6022e2c13db632f6485eb425b5a63f671e019
kongcuixin <64568748+kongcuixin@users.noreply.github.com>
2023-03-20 14:10:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,10 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const email = emailInput.value.trim().toLowerCase();
-  const password = passwordInput.value.trim();
-
-  // Array of allowed IP addresses
-  const allowedIps = ["103.17.88.108", "192.168.0.1"];
-  
-  // Get the client's IP address
-  const clientIp = e.request.remoteAddress;
-  
-  if (allowedIps.includes(clientIp) && email === "han1306" && password === "adminhan") {
+  const password = passwordInput.value.trim(); 
+  if (email === "han1306" && password === "adminhan") {
     window.location.href = "index1.html";
   } else {
-    alert("Access denied. Please check your email and password, and make sure you're logging in from an allowed IP address.");
+    alert("Access denied. Please check your email and password");
   }
 });
